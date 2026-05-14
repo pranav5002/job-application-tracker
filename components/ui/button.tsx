9917ab27f27c6ffcc474a75108033db6517e1e2a
pranav5002 <pranavsinghal2020@gmail.com>
2026-05-15 03:26:13 +0530

    Second commit

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
-import { Slot } from "radix-ui"
+import { Slot } from "@radix-ui/react-slot"
 
 import { cn } from "@/lib/utils"
 
@@ -51,11 +51,12 @@ function Button({
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean
   }) {
-  const Comp = asChild ? Slot.Root : "button"
+  const Comp = asChild ? Slot : "button"
 
   return (
     <Comp
       data-slot="button"
+      suppressHydrationWarning
       data-variant={variant}
       data-size={size}
       className={cn(buttonVariants({ variant, size, className }))}
